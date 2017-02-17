faf1381071ff57487bc92416b75dd3d5696ceebd
thiago <thiagoprocaci@gmail.com>
2017-02-17 00:34:28 -0200

    weighted sun

--- A/reputation.R
+++ A/reputation.R
@@ -6,6 +6,9 @@ source("../functions/Functions.R")
 #change the file path
 data <- read.csv(file = "user.csv", header = TRUE, sep = ";")
 
+data[, "weightedSum"] =  ((0.7 * data$answers) + (0.6 * data$answer_comments) + (0.5 * data$question_comments)  )
+data[, "weightedSum2"] =  ((0.7 * data$answers) + (0.6 * data$answer_comments) + (0.5 * data$question_comments)) / (data$questions + 1)
+
 print("Summary reputation")
 print(summary(data$reputation))
 
@@ -25,5 +28,12 @@ correlation(nonExpertData$reputation, nonExpertData$answer_comments, "Reputation
 correlation(expertsData$reputation, expertsData$question_comments, "Reputation X Question Comments - Experts")
 correlation(nonExpertData$reputation, nonExpertData$question_comments, "Reputation X Question Comments - Non Experts")
 
+
+
+correlation(expertsData$reputation, expertsData$weightedSum, "Reputation X  Weighted Sum - Experts")
+correlation(nonExpertData$reputation, nonExpertData$weightedSum, "Reputation X Weighted Sum - Non Experts")
+
+correlation(expertsData$reputation, expertsData$weightedSum2, "Reputation X  Weighted Sum 2 - Experts")
+correlation(nonExpertData$reputation, nonExpertData$weightedSum2, "Reputation X Weighted Sum 2 - Non Experts")
 
 
